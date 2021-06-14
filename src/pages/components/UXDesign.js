c841e20c682755bb452a2a6ac3eafe905aba111d
ToastyMartian <cakallemein@gmail.com>
2021-06-14 22:12:40 +0200

    Added text to the user research card in uxdesign.js

--- src/pages/components/UXDesign.js
+++ src/pages/components/UXDesign.js
@@ -6,7 +6,7 @@ function UXDesign() {
             <section className='bg-cyan-dark mx-8 rounded-tl-large mt-10'>
                 <h2 className='text-center py-5'>Prototypes and Wireframes</h2>
                 <div className='text-purple-light font-thin max-w-prose m-auto'>
-                    <h4>In order to begin designing a good looking webpage or game assets, you first need to make wireframes and prototypes to be able to test the design with your users. That way the design can easily be changed early on in the design process, which can save a lot of time.</h4>
+                    <h4>In order to begin designing a good looking webpage, UI or game assets, you first need to make wireframes and prototypes to be able to test the design with your users. That way the design can easily be changed early on in the design process, which can save a lot of time.</h4>
                     <h4 className='py-5'>Because it is a significant part of the design process, I have created wireframes and/or prototypes for nearly each project that i've done in the past.</h4>
                     <h4 className='py-5'>Below are some visual examples of these prototypes and wireframes and how i go about designing them and optimizing them for testing.</h4>
                 </div>
@@ -16,10 +16,10 @@ function UXDesign() {
             <section className='bg-cyan-dark mx-8 rounded-tl-large mt-20'>
                 <h2 className='text-center py-5'>User Research</h2>
                 <div className='text-purple-light font-thin max-w-prose m-auto'>
-                    <h4>Lorem ipsum</h4> 
-                    <h4 className='py-5'>Lorem ipsum</h4>
-                    <h4 className='py-5'>Lorem ipsum</h4>
-                    <h4 className='py-5'>Lorem ipsum</h4>
+                    <h4>In order to create the best user experience possible, it is useful to conduct the research neccessary to find out more about your target audience and what they want to see in your product(s).</h4> 
+                    <h4 className='py-5'>To accomplish this, you could interviews and tests with the users, but you could also do some desk research, something that has grown in popularity since the recent developments surrounding the Covid pandemic.</h4>
+                    <h4 className='py-5'>I have done a variety of these in my projects, mostly depending on what type of research was most applicable to the project and its goals.</h4>
+                    <h4 className='py-5'>Below are some examples of the results and how me and my teammates ended up using the information we got from this research.</h4>
                 </div>
                 <h3 className='ml-10 pb-5'>Gallery</h3>
             </section>
