8ca7e174ed011c245ba37a36bec1c85c6bf9440a
ToastyMartian <cakallemein@gmail.com>
2021-06-14 21:48:53 +0200

    Updated text for the prototypes and wireframes card

--- src/pages/components/UXDesign.js
+++ src/pages/components/UXDesign.js
@@ -6,9 +6,9 @@ function UXDesign() {
             <section className='bg-cyan-dark mx-8 rounded-tl-large mt-10'>
                 <h2 className='text-center py-5'>Prototypes and Wireframes</h2>
                 <div className='text-purple-light font-thin max-w-prose m-auto'>
-                    <h4>Lorem ipsum</h4>
-                    <h4 className='py-5'>Lorem ipsum</h4>
-                    <h4 className='py-5'>Lorem ipsum</h4>
+                    <h4>In order to begin designing a good looking webpage or game assets, you first need to make wireframes and prototypes to be able to test the design with your users. That way the design can easily be changed early on in the design process, which can save a lot of time.</h4>
+                    <h4 className='py-5'>Because it is a significant part of the design process, I have created wireframes and/or prototypes for nearly each project that i've done in the past.</h4>
+                    <h4 className='py-5'>Below are some visual examples of these prototypes and wireframes and how i go about designing them and optimizing them for testing.</h4>
                 </div>
                 <h3 className='ml-10 pb-5'>Gallery</h3>
             </section>
